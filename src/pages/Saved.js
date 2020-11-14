a026a1cd68da9eb8133140e4ad9e938db3242e90
Max Hanson <mthanson2@gmail.com>
2020-11-13 17:22:05 -0800

    Changed server

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import DeleteBtn from "../components/DeleteBtn";
-// import Jumbotron from "../components/Jumbotron";
-import API from "../routes/api/API";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../components/Grid";
-// import { List, ListItem } from "../components/List";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../../routes/api/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Books() {
